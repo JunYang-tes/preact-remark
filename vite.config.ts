9b73a63fcd8b8afb8982b1a9e767bfd239a5cadc
YJ <jun.yang@target-energysolutions.com>
2025-04-27 13:38:28 +0800

    update build script

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
+import {resolve} from 'path'
 import preact from '@preact/preset-vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [preact()],
+  build: {
+    lib:{
+      entry: resolve(__dirname, 'src/index.ts'),
+      formats: ['es']
+    },
+    rollupOptions: {
+      external(src,importer,isResolved) {
+        if(src.includes('preact-remark')) return false
+        return true
+      }
+    }
+  }
 })
